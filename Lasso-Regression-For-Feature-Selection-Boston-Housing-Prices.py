402b3a63745bf44b1427b3f78189d91142ea881e
Antony Helson Chandy <59191453+AntonyHelsonChandy@users.noreply.github.com>
2020-03-25 06:51:24 +0000

    Update Lasso-Regression-For-Feature-Selection-Boston-Housing-Prices.py

--- Lasso-Regression-For-Feature-Selection-Boston-Housing-Prices.py
+++ Lasso-Regression-For-Feature-Selection-Boston-Housing-Prices.py
@@ -13,22 +13,24 @@ boston_y = pd.DataFrame(bostona.target, columns = ['MEDV'])
 boston_y
 print(boston_y.head())
 
-import seaborn as sns
 
-g = sns.pairplot(boston_pd)
+#import seaborn as sns
+#g = sns.pairplot(boston_pd)
+
+
 
 from sklearn import linear_model
 
 clf = linear_model.Lasso(alpha=0.1)
 clf.fit(boston_pd,boston_y)
+
 clf.coef_
 print(clf.coef_)
 
-import matplotlib.pyplot as plt
 
+import matplotlib.pyplot as plt
 _=plt.plot(range(len(bostona.feature_names)),clf.coef_)
 _=plt.xticks(range(len(bostona.feature_names)),bostona.feature_names,rotation=60)
 _=plt.ylabel("Coefficients")
+
 plt.show()
-
-
